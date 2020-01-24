d2383d57db14d23c75b241432f2874e5e77a965f
Anthony Monthe <anthony.monthe@gmail.com>
2020-01-24 22:59:23 +0000

    Added Drone

---
+++ drone_install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo=$(which sudo)
+
+if [[ "$DRONE_STAGE_OS" == "linux" ]] ; then
+    export DEBIAN_FRONTEND=noninteractive
+    $sudo apt -y -qq update
+    $sudo apt install -y -q python3 jq wget
+elif [[ "$DRONE_STAGE_OS" == "darwin" ]] ; then
+    brew upgrade ruby
+    brew install jq wget
+fi
+
+echo -e "{
+  \"images\": {
+      \"amd64_docker_linux\": \"$AMD64_DOCKER_LINUX_IMAGE_ID\",
+      \"arm64_docker_linux\": \"$ARM64_DOCKER_LINUX_IMAGE_ID\"
+  },
+  \"flavors\": {
+      \"amd64_docker\": \"$AMD64_DOCKER_FLAVOR_ID\",
+      \"arm64_docker\": \"$ARM64_DOCKER_FLAVOR_ID\" 
+  }
+}" > .infra-opts.json
+
+image_id=$(jq .images.${DRONE_STAGE_ARCH}_${DRONE_STAGE_TYPE}_${DRONE_STAGE_OS} .infra-opts.json)
+flavor_id=$(jq .flavors.${DRONE_STAGE_ARCH}_${DRONE_STAGE_TYPE} .infra-opts.json)
+
+echo -e "{
+  \"master_url\": \"$MASTER_URL\",
+  \"token\": \"$TOKEN\",
+  \"provider\": \"$PROVIDER_ID\",
+  \"datacenter\": \"$DATACENTER_ID\",
+  \"flavor\": $flavor_id,
+  \"image\": $image_id
+}" > .cb_client.json
